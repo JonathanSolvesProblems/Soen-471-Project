b31d6f7b9387c80803ccb550a91d7ef912a4fce1
andrewha-dev <andrew.ha.dev@gmail.com>
2021-04-02 22:59:19 -0400

    Code cleanup

--- plot.py
+++ plot.py
@@ -2,25 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import FormatStrFormatter
 def plotUpvotes(df, bins):
-    # Show histogram of the 'C1' column
     bins, counts = df.select('upvotes').rdd.flatMap(lambda x: x).histogram(bins)
     fig, ax = plt.subplots()
-    # plt.hist(bins[:-1], bins=bins, weights=counts)
-    # plt.show()
-    freq, bins, patches = plt.hist(bins[:-1], edgecolor='white', label='d', bins=bins, weights=counts)
+
+    freq, bins, patches = plt.hist(bins[:-1], edgecolor='white', label='1231231', bins=bins, weights=counts)
+    plt.xlabel('Upvotes')
+    plt.ylabel('Count')
+    plt.title('Upvote Distribution')
     counts = np.array(counts)
     # Set the ticks to be at the edges of the bins.
     ax.set_xticks(bins)
     # Set the xaxis's tick labels to be formatted with 1 decimal place...
     ax.xaxis.set_major_formatter(FormatStrFormatter('%0.1f'))
-
-    # Change the colors of bars at the edges...
-    twentyfifth, seventyfifth = np.percentile(counts, [25, 75])
-    for patch, rightside, leftside in zip(patches, bins[1:], bins[:-1]):
-        if rightside < twentyfifth:
-            patch.set_facecolor('green')
-        elif leftside > seventyfifth:
-            patch.set_facecolor('red')
 
     # Label the raw counts and the percentages below the x-axis...
     bin_centers = 0.5 * np.diff(bins) + bins[:-1]
